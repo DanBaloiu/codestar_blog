b47cf8ecb4af4806de6806125fe57fe0dad12207
Dan Baloiu <dan@Dans-MacBook-Pro.local>
2025-10-07 15:51:14 +0300

    Add excerpt to base.html

---
+++ env.py
@@ -0,0 +1,6 @@
+import os
+
+os.environ.setdefault(
+    "DATABASE_URL", "postgresql://neondb_owner:npg_Ftd5lLN7sqgv@ep-bold-bread-agig7f71.c-2.eu-central-1.aws.neon.tech/left_tart_stain_353628"
+)
+os.environ.setdefault("SECRET_KEY", "$9h76#x5p_f0!ix=92n$$%95)qdd#n%h#(qd(qoinqn+yi_jpk")
