7dca835a3ccb3b35c30cc99a4babd7b30411d230
Dan Baloiu <dan@Dans-MacBook-Pro.local>
2025-10-11 19:25:01 +0100

    Enable serving of image files

--- env.py
+++ env.py
@@ -4,3 +4,8 @@ os.environ.setdefault(
     "DATABASE_URL", "postgresql://neondb_owner:npg_Ftd5lLN7sqgv@ep-bold-bread-agig7f71.c-2.eu-central-1.aws.neon.tech/left_tart_stain_353628"
 )
 os.environ.setdefault("SECRET_KEY", "$9h76#x5p_f0!ix=92n$$%95)qdd#n%h#(qd(qoinqn+yi_jpk")
+
+os.environ.setdefault(
+    "CLOUDINARY_URL", "cloudinary://442251418468745:VA-aK18Yt14aL4uVYZ_PDy2YKWM@dtblf2tm2")
+
+
